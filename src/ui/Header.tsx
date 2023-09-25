7dcf66ab480e6de90c917fc48ab7509f2d268554
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-09-25 21:13:02 +0200

    feat: modern react router loaders and actions loaders for fetching data, actions for writing data

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/searchOrder';
 
 export default function Header() {
   return (
     <header>
-      <Link to={"/"}>Fast Pizza Co.</Link>
+      <Link to={'/'}>Fast Pizza Co.</Link>
+      <SearchOrder />
 
       <p>Gracjan</p>
     </header>
-  )
+  );
 }
