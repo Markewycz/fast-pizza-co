025a7708880e10e067fbc5de2c30597f22f646af
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-09-30 20:24:18 +0200

    feat: tailwind css implementation

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -5,14 +5,17 @@ import MenuItem from './MenuItem';
 function Menu() {
   const menu = useLoaderData();
 
-  return <ul>
-    {menu.map(pizza => <MenuItem key={pizza.id} pizza={pizza} />)}
-  </ul>;
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
 
 export async function loader() {
   const menu = await getMenu();
-
   return menu;
 }
 
